0bd70c03413bd4ef8faa1f6f5c2b91993f86544c
Rowan Fortier <rowan.fortier@gmail.com>
2023-04-03 17:49:00 -0700

    Add Lunar Regolith

--- src/main/java/blobinati/stellamagnus/block/ModBlocks.java
+++ src/main/java/blobinati/stellamagnus/block/ModBlocks.java
@@ -5,18 +5,18 @@ import blobinati.stellamagnus.item.ModItemGroup;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.ExperienceDroppingBlock;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.sound.BlockSoundGroup;
 
-public class ModBlocks {
+public class ModBlocks{
 
     public static final Block ZIRCONIUM_BLOCK = registerBlock("zirconium_block",
         new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool()), ModItemGroup.STELLAMAGNUS);
@@ -25,6 +25,9 @@ public class ModBlocks {
     public static final Block LUNAR_BASALT_ZIRCONIUM_ORE = registerBlock("lunar_basalt_zirconium_ore",
             new ExperienceDroppingBlock(FabricBlockSettings.of(Material.STONE).strength(4.0f).requiresTool(),
                 UniformIntProvider.create(3,7)), ModItemGroup.STELLAMAGNUS);
+    public static final FallingBlock LUNAR_REGOLITH = (FallingBlock) registerBlock("lunar_regolith",
+            new FallingBlock(AbstractBlock.Settings.of(Material.AGGREGATE).strength(4.0f).requiresTool().sounds(BlockSoundGroup.SAND)), ModItemGroup.STELLAMAGNUS);
+            //new SandBlock(FabricBlockSettings.of(Material.SOIL).strength(4.0f).requiresTool()).sounds(BlockSoundGroup.SAND), ModItemGroup.STELLAMAGNUS);
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
